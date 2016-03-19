13b11997eb2157ef877b155031fa6a095eddcabc
Edwin Martinez <tusoftwarepersonal@gmail.com>
2016-03-18 23:26:43 -0400

    Uploading files.

---
+++ DW_SQL_Editor/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace DW_SQL_Editor
+{
+    partial class frmMain
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.tsmFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmOpenFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmSaveFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.tsmAbout = new System.Windows.Forms.ToolStripMenuItem();
+            this.boxCode = new System.Windows.Forms.RichTextBox();
+            this.btnOpenFile = new System.Windows.Forms.Button();
+            this.btnSaveFile = new System.Windows.Forms.Button();
+            this.dlgOpenFile = new System.Windows.Forms.OpenFileDialog();
+            this.dlgSaveFile = new System.Windows.Forms.SaveFileDialog();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmFile,
+            this.tsmAbout});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(728, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // tsmFile
+            // 
+            this.tsmFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsmOpenFile,
+            this.tsmSaveFile,
+            this.tsmExit});
+            this.tsmFile.Name = "tsmFile";
+            this.tsmFile.Size = new System.Drawing.Size(35, 20);
+            this.tsmFile.Text = "File";
+            // 
+            // tsmOpenFile
+            // 
+            this.tsmOpenFile.Name = "tsmOpenFile";
+            this.tsmOpenFile.Size = new System.Drawing.Size(131, 22);
+            this.tsmOpenFile.Text = "Open File...";
+            this.tsmOpenFile.Click += new System.EventHandler(this.tsmOpenFile_Click);
+            // 
+            // tsmSaveFile
+            // 
+            this.tsmSaveFile.Name = "tsmSaveFile";
+            this.tsmSaveFile.Size = new System.Drawing.Size(131, 22);
+            this.tsmSaveFile.Text = "Save File...";
+            this.tsmSaveFile.Click += new System.EventHandler(this.tsmSaveFile_Click);
+            // 
+            // tsmExit
+            // 
+            this.tsmExit.Name = "tsmExit";
+            this.tsmExit.Size = new System.Drawing.Size(131, 22);
+            this.tsmExit.Text = "Exit";
+            this.tsmExit.Click += new System.EventHandler(this.tsmExit_Click);
+            // 
+            // tsmAbout
+            // 
+            this.tsmAbout.Name = "tsmAbout";
+            this.tsmAbout.Size = new System.Drawing.Size(48, 20);
+            this.tsmAbout.Text = "About";
+            this.tsmAbout.Click += new System.EventHandler(this.tsmAbout_Click);
+            // 
+            // boxCode
+            // 
+            this.boxCode.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.boxCode.Location = new System.Drawing.Point(164, 28);
+            this.boxCode.Name = "boxCode";
+            this.boxCode.Size = new System.Drawing.Size(552, 357);
+            this.boxCode.TabIndex = 1;
+            this.boxCode.Text = "";
+            // 
+            // btnOpenFile
+            // 
+            this.btnOpenFile.Location = new System.Drawing.Point(12, 51);
+            this.btnOpenFile.Name = "btnOpenFile";
+            this.btnOpenFile.Size = new System.Drawing.Size(114, 41);
+            this.btnOpenFile.TabIndex = 2;
+            this.btnOpenFile.Text = "Open SQL File";
+            this.btnOpenFile.UseVisualStyleBackColor = true;
+            this.btnOpenFile.Click += new System.EventHandler(this.btnOpenFile_Click);
+            // 
+            // btnSaveFile
+            // 
+            this.btnSaveFile.Location = new System.Drawing.Point(12, 128);
+            this.btnSaveFile.Name = "btnSaveFile";
+            this.btnSaveFile.Size = new System.Drawing.Size(115, 41);
+            this.btnSaveFile.TabIndex = 3;
+            this.btnSaveFile.Text = "Save SQL File";
+            this.btnSaveFile.UseVisualStyleBackColor = true;
+            this.btnSaveFile.Click += new System.EventHandler(this.btnSaveFile_Click);
+            // 
+            // frmMain
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(728, 397);
+            this.Controls.Add(this.btnSaveFile);
+            this.Controls.Add(this.btnOpenFile);
+            this.Controls.Add(this.boxCode);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "frmMain";
+            this.Text = "DW SQL Editor";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem tsmFile;
+        private System.Windows.Forms.ToolStripMenuItem tsmOpenFile;
+        private System.Windows.Forms.ToolStripMenuItem tsmSaveFile;
+        private System.Windows.Forms.ToolStripMenuItem tsmExit;
+        private System.Windows.Forms.ToolStripMenuItem tsmAbout;
+        private System.Windows.Forms.RichTextBox boxCode;
+        private System.Windows.Forms.Button btnOpenFile;
+        private System.Windows.Forms.Button btnSaveFile;
+        private System.Windows.Forms.OpenFileDialog dlgOpenFile;
+        private System.Windows.Forms.SaveFileDialog dlgSaveFile;
+    }
+}
+
---
+++ DW_SQL_Editor/Form1.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DW_SQL_Editor
+{
+    public partial class frmMain : Form
+    {
+        public frmMain()
+        {
+            InitializeComponent();
+        }
+
+        private void tsmAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("DW SQL Editor is a free tool to edit your SQL files.", "About DW SQL Editor", MessageBoxButtons.OK);
+        }
+
+        private void tsmExit_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Thanks for using this simple app.", "DW SQL Editor", MessageBoxButtons.OK);
+            Application.Exit();
+        }
+
+        private void tsmSaveFile_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void tsmOpenFile_Click(object sender, EventArgs e)
+        {
+            dlgOpenFile.InitialDirectory = @"C:\";
+            dlgOpenFile.Title = "Browse for the SQL File";
+            dlgOpenFile.CheckFileExists = true;
+            dlgOpenFile.CheckPathExists = true;
+            dlgOpenFile.DefaultExt = "sql";
+            dlgOpenFile.Filter = "SQL Files|*.sql|Text Files|*.txt";
+            if (dlgOpenFile.ShowDialog() == DialogResult.OK)
+            {
+                boxCode.Text = File.ReadAllText(dlgOpenFile.FileName);
+            }
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e)
+        {
+            dlgOpenFile.InitialDirectory = @"C:\";
+            dlgOpenFile.Title = "Browse for the SQL File";
+            dlgOpenFile.CheckFileExists = true;
+            dlgOpenFile.CheckPathExists = true;
+            dlgOpenFile.DefaultExt = "sql";
+            dlgOpenFile.Filter = "SQL Files|*.sql|Text Files|*.txt";
+            if (dlgOpenFile.ShowDialog()==DialogResult.OK)
+            {
+                boxCode.Text = File.ReadAllText(dlgOpenFile.FileName);
+            }
+        }
+
+        //Memory Stream for saving file 
+        MemoryStream fstream = new MemoryStream();
+
+        //Saving content from the TextBox
+        private void btnSaveFile_Click(object sender, EventArgs e)
+        {
+            RichTextBox boxcode = new RichTextBox();
+            boxcode.SaveFile(fstream, RichTextBoxStreamType.UnicodePlainText);
+            fstream.WriteByte(13);
+            SaveFileDialog dlgSaveFile = new SaveFileDialog();
+            dlgSaveFile.CreatePrompt = true;
+            dlgSaveFile.OverwritePrompt = true;
+            dlgSaveFile.FileName = boxcode.Text;
+            dlgSaveFile.Filter = "SQL files (*.sql)|*.sql";
+            dlgSaveFile.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+            DialogResult result = dlgSaveFile.ShowDialog();
+            Stream fileStream;
+
+            if (result == DialogResult.OK)
+            {
+                // Open the file and copy it's content to the stream
+                // Starts at position 0 of the stream
+                // Then it closed and saved
+                fileStream = dlgSaveFile.OpenFile();
+                fstream.Position = 0;
+                fstream.WriteTo(fileStream);
+                fileStream.Close();
+            }
+        }
+
+    }
+}
